8a2f1d7ad012328e5471f5a271083a6598b7edc8
Fabio <fabiodebenedetto05@gmail.com>
2026-01-21 11:40:22 +0100

    Partial Employee class and functions deploy

--- include/Employee.h
+++ include/Employee.h
@@ -1,1 +1,27 @@
+#include <string>
+
+using namespace std;
+
+class Employee{
+    private:
+        int id;
+        string name, surname, role;
+        double hourlyRate, hoursWorked, taxRate;
+
+    public:
+        Employee(int i, const string n, const string s, const string r, double rate, double tax){
+            id = i;
+            name = n;
+            surname = s;
+            role = r;
+            hourlyRate = rate;
+            hoursWorked = 0.0;
+            taxRate = tax;
+        }
+        void addHours(double h);
+
+        double calculateGrossSalary() const;
+
+        double calculateNetSalary() const;
+};
 
--- src/Employee.cpp
+++ src/Employee.cpp
@@ -1,1 +1,15 @@
+#include "Employee.h"
 
+void Employee::addHours(double h){
+            if (h > 0)
+                hoursWorked += h;
+        }
+
+double Employee::calculateGrossSalary() const{
+    return hoursWorked * hourlyRate;
+}
+
+double Employee::calculateNetSalary() const{
+    double netFactor = 1 - taxRate;
+    return (calculateGrossSalary() * netFactor);
+}
